f9307dd8654677f214de04cc24ccc801be073c90
zYenAnh <20520772@gm.uit.edu.vn>
2022-05-13 23:27:55 +0700

    Connect

--- StudentManagement/src/test/Test.java
+++ StudentManagement/src/test/Test.java
@@ -7,7 +7,6 @@ import view.QLSVView;
 
 public class Test {
 	public static void main(String[] args) {
-		new QLSVView();
 		try {
 			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 			new QLSVView();
